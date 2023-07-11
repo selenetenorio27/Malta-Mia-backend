4a2895b8218e8e2b0ba3cb5b937e0fa88fe6f777
Sel <selenetenorio27@hotmail.com>
2023-07-11 11:20:15 -0700

    Project structure added

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI")
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    CORS(app)
+    return app
