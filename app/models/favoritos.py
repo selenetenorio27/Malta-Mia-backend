dd90ebb5c6b9a3628a922143675eec73d3d8c4cb
Sel <selenetenorio27@hotmail.com>
2023-08-10 19:35:25 -0700

    Favoritos model changed, client_id to string instead of int

--- app/models/favoritos.py
+++ app/models/favoritos.py
@@ -2,7 +2,7 @@ from app import db
 
 class Favoritos(db.Model):
     favoritos_id = db.Column(db.Integer, primary_key=True)
-    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.cliente_id'))
+    cliente_id = db.Column(db.String, db.ForeignKey('cliente.cliente_id'))
     cerveza_id = db.Column(db.Integer, db.ForeignKey('cerveza.cerveza_id'))
 
     cliente = db.relationship('Cliente',back_populates='favoritos')
